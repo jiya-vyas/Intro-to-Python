b6e5311b708da6ee2ea38019a1f66a3143dc28a3
jiya-vyas <vyasdhara21@gmail.com>
2024-10-17 20:00:02 -0400

    Add files via upload

---
+++ 1.7.py
@@ -0,0 +1,53 @@
+import turtle
+##screen = turtle.Screen() # create a Screen object
+##screen.setup(600,600) # big square screen
+##screen.bgcolor("red") # background colour
+##jaymit = turtle.Turtle()
+##jaymit.forward(300)
+##screen.bgcolor("orange")
+##jaymit.left(180)
+##jaymit.forward(600) # when jaymit hits the wall
+##screen.bgcolor("yellow") # the background colour changes
+##screen.reset()
+
+shelly=turtle.Turtle()
+##shelly.color("black")
+##shelly.pensize(5)
+##shelly.fillcolor("red")
+##shelly.begin_fill()
+##shelly.circle(100)
+##shelly.end_fill()
+##
+##shelly.left(60)
+##shelly.fillcolor("orange")
+##shelly.begin_fill()
+##shelly.circle(100)
+##shelly.end_fill()
+##
+##shelly.left(60)
+##shelly.fillcolor("yellow")
+##shelly.begin_fill()
+##shelly.circle(100)
+##shelly.end_fill()
+##shelly.left(60)
+##shelly.fillcolor("green")
+##shelly.begin_fill()
+##shelly.circle(100)
+##shelly.end_fill()
+##shelly.left(60)
+##shelly.color("blue")
+##shelly.begin_fill()
+##shelly.circle(100)
+##shelly.end_fill()
+##shelly.left(60)
+##shelly.fillcolor("purple")
+##shelly.begin_fill()
+##shelly.circle(100)
+##shelly.end_fill()
+##
+##
+
+shelly
+
+
+
